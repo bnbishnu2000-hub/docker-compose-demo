8d940076e0e0a55586e19e755eaba180d8c93eb1
Bishnu Panigrahi <bnbishnu2000@gmail.com>
2026-05-13 01:48:48 +0530

    Docker compose file created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+ 
+const app = express();
+ 
+// connect to MongoDB (service name = db)
+mongoose.connect('mongodb://db:27017/test')
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.log(err));
+ 
+app.get('/', (req, res) => {
+  res.send("Hello from Docker Compose 🚀");
+});
+ 
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
